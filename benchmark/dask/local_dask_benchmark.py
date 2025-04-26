aa3acd2f0319c2620d9f4292abb2fe022055b221
Robert Gleison <robertgleison12@gmail.com>
2025-04-26 16:10:26 +0100

    feat: added local dask benchmark

---
+++ benchmark/dask/local_dask_benchmark.py
@@ -0,0 +1,49 @@
+from dask.distributed import Client
+from benchmark import benchmark
+import dask.dataframe as dd
+import pandas as pd
+import numpy as np
+from benchmark.dask.tasks import (
+    read_file_parquet,
+    count,
+    count_index_length,
+    mean,
+    standard_deviation,
+    mean_of_sum,
+    sum_columns,
+    mean_of_product,
+    product_columns,
+    value_counts,
+    mean_of_complicated_arithmetic_operation,
+    complicated_arithmetic_operation,
+    groupby_statistics,
+    join_count,
+    join_data,
+)
+
+
+client = Client()
+dask_data = dd.read_parquet('/dbfs/FileStore/ks_taxi_parquet', index='index')
+dask_benchmarks = {
+    'duration': [],  # in seconds
+    'task': [],
+}
+
+other = groupby_statistics(dask_data)
+other.columns = pd.Index([e[0]+'_' + e[1] for e in other.columns.tolist()])
+
+benchmark(read_file_parquet, df=None, benchmarks=dask_benchmarks, name='read file')
+benchmark(count, df=dask_data, benchmarks=dask_benchmarks, name='count')
+benchmark(count_index_length, df=dask_data, benchmarks=dask_benchmarks, name='count index length')
+benchmark(mean, df=dask_data, benchmarks=dask_benchmarks, name='mean')
+benchmark(standard_deviation, df=dask_data, benchmarks=dask_benchmarks, name='standard deviation')
+benchmark(mean_of_sum, df=dask_data, benchmarks=dask_benchmarks, name='mean of columns addition')
+benchmark(sum_columns, df=dask_data, benchmarks=dask_benchmarks, name='addition of columns')
+benchmark(mean_of_product, df=dask_data, benchmarks=dask_benchmarks, name='mean of columns multiplication')
+benchmark(product_columns, df=dask_data, benchmarks=dask_benchmarks, name='multiplication of columns')
+benchmark(value_counts, df=dask_data, benchmarks=dask_benchmarks, name='value counts')
+benchmark(mean_of_complicated_arithmetic_operation, df=dask_data, benchmarks=dask_benchmarks, name='mean of complex arithmetic ops')
+benchmark(complicated_arithmetic_operation, df=dask_data, benchmarks=dask_benchmarks, name='complex arithmetic ops')
+benchmark(groupby_statistics, df=dask_data, benchmarks=dask_benchmarks, name='groupby statistics')
+benchmark(join_count, dask_data, benchmarks=dask_benchmarks, name='join count', other=other)
+benchmark(join_data, dask_data, benchmarks=dask_benchmarks, name='join', other=other)
