14299007ed0ee59b84e5b4639986653f5be4d64c
Gabriela <gabrielascenco@gmail.com>
2025-04-26 17:40:11 +0100

    joblib local files

---
+++ benchmark/joblib/local_with_filtering_and_cache_joblib_benchmark.py
@@ -0,0 +1,55 @@
+from benchmark import benchmark
+from distributed import wait
+import pandas as pd
+import numpy as np
+from benchmark.joblib.tasks import (
+    read_file_parquet,
+    count,
+    count_index_length,
+    mean,
+    standard_deviation,
+    mean_of_sum,
+    sum_columns,
+    mean_of_product,
+    product_columns,
+    value_counts,
+    mean_of_complicated_arithmetic_operation,
+    complicated_arithmetic_operation,
+    groupby_statistics,
+    join_count,
+    join_data,
+)
+
+joblib_data = read_file_parquet().compute()
+
+joblib_benchmarks = {
+    'duration': [],  # in seconds
+    'task': [],
+}
+
+expr_filter = (joblib_data.tip_amt >= 1) & (joblib_data.tip_amt <= 5)
+joblib_filtered = joblib_data[expr_filter]
+
+# Add caching
+joblib_filtered = read_file_parquet().compute()
+print('Waiting until all futures are finished')
+wait(joblib_filtered)
+print('All futures are finished')
+
+benchmark(count, joblib_filtered, benchmarks=joblib_benchmarks, name='filtered count')
+benchmark(count_index_length, joblib_filtered, benchmarks=joblib_benchmarks, name='filtered count index length')
+benchmark(mean, joblib_filtered, benchmarks=joblib_benchmarks, name='filtered mean')
+benchmark(standard_deviation, joblib_filtered, benchmarks=joblib_benchmarks, name='filtered standard deviation')
+benchmark(mean_of_sum, joblib_filtered, benchmarks=joblib_benchmarks, name ='filtered mean of columns addition')
+benchmark(sum_columns, df=joblib_filtered, benchmarks=joblib_benchmarks, name='filtered addition of columns')
+benchmark(mean_of_product, joblib_filtered, benchmarks=joblib_benchmarks, name ='filtered mean of columns multiplication')
+benchmark(product_columns, df=joblib_filtered, benchmarks=joblib_benchmarks, name='filtered multiplication of columns')
+benchmark(mean_of_complicated_arithmetic_operation, joblib_filtered, benchmarks=joblib_benchmarks, name='filtered mean of complex arithmetic ops')
+benchmark(complicated_arithmetic_operation, joblib_filtered, benchmarks=joblib_benchmarks, name='filtered complex arithmetic ops')
+benchmark(value_counts, joblib_filtered, benchmarks=joblib_benchmarks, name ='filtered value counts')
+benchmark(groupby_statistics, joblib_filtered, benchmarks=joblib_benchmarks, name='filtered groupby statistics')
+
+other = groupby_statistics(joblib_filtered)
+other.columns = pd.Index([e[0]+'_' + e[1] for e in other.columns.tolist()])
+benchmark(join_count, joblib_filtered, benchmarks=joblib_benchmarks, name='filtered join count', other=other)
+benchmark(join_data, joblib_filtered, benchmarks=joblib_benchmarks, name='filtered join', other=other)
---
+++ benchmark/joblib/tasks.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import numpy as np
+import dask.dataframe as dd
+from joblib import delayed
+
+@delayed
+def read_file_parquet(df=None):
+    return dd.read_parquet('/dbfs/FileStore/ks_taxi_parquet', index='index')
+
+@delayed
+def count(df=None):
+    return len(df)
+
+@delayed
+def count_index_length(df=None):
+    return len(df.index)
+
+@delayed
+def mean(df):
+    return df.fare_amt.mean().compute()
+
+@delayed
+def standard_deviation(df):
+    return df.fare_amt.std().compute()
+
+@delayed
+def mean_of_sum(df):
+    return (df.fare_amt + df.tip_amt).mean().compute()
+
+@delayed
+def sum_columns(df):
+    return (df.fare_amt + df.tip_amt).compute()
+
+@delayed
+def mean_of_product(df):
+    return (df.fare_amt * df.tip_amt).mean().compute()
+
+@delayed
+def product_columns(df):
+    return (df.fare_amt * df.tip_amt).compute()
+
+@delayed
+def value_counts(df):
+    return df.fare_amt.value_counts().compute()
+
+@delayed
+def mean_of_complicated_arithmetic_operation(df):
+    theta_1 = df.start_lon
+    phi_1 = df.start_lat
+    theta_2 = df.end_lon
+    phi_2 = df.end_lat
+    temp = (np.sin((theta_2 - theta_1) / 2 * np.pi / 180) ** 2
+           + np.cos(theta_1 * np.pi / 180) * np.cos(theta_2 * np.pi / 180)
+           * np.sin((phi_2 - phi_1) / 2 * np.pi / 180) ** 2)
+    ret = 2 * np.arctan2(np.sqrt(temp), np.sqrt(1 - temp))
+    return ret.mean().compute()
+
+@delayed
+def complicated_arithmetic_operation(df):
+    theta_1 = df.start_lon
+    phi_1 = df.start_lat
+    theta_2 = df.end_lon
+    phi_2 = df.end_lat
+    temp = (np.sin((theta_2 - theta_1) / 2 * np.pi / 180) ** 2
+           + np.cos(theta_1 * np.pi / 180) * np.cos(theta_2 * np.pi / 180)
+           * np.sin((phi_2 - phi_1) / 2 * np.pi / 180) ** 2)
+    ret = 2 * np.arctan2(np.sqrt(temp), np.sqrt(1 - temp))
+    return ret.compute()
+
+@delayed
+def groupby_statistics(df):
+    return df.groupby(by='passenger_count').agg(
+        {
+            'fare_amt': ['mean', 'std'],
+            'tip_amt': ['mean', 'std']
+        }
+    ).compute()
+
+@delayed
+def join_count(df, other):
+    return len(dd.merge(df, other, left_index=True, right_index=True))
+
+@delayed
+def join_data(df, other):
+    return dd.merge(df, other, left_index=True, right_index=True).compute()
